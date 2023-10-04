6f16124926f180c5b3563c67e84f214c12c10eb0
Filipp Kolesnikov <filippp0@yandex.ru>
2023-10-05 00:40:39 +0300

    main correct

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,7 +11,21 @@ import Profile from '../Profile/Profile';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Movies from '../Movies/Movies';
 
-export default function Main({ name, onRegister, onLogin, logOut, editUserData, setIsError, savedMovies, onDelete, addMovie, isSuccess, setSuccess, setIsEdit, isEdit }) {
+export default function Main({
+  name,
+  onRegister,
+  onLogin,
+  logOut,
+  editUserData,
+  setIsError,
+  savedMovies,
+  onDelete,
+  addMovie,
+  isSuccess,
+  setSuccess,
+  setIsEdit,
+  isEdit
+}) {
 
   return (
     <main className="main">
@@ -28,15 +42,15 @@ export default function Main({ name, onRegister, onLogin, logOut, editUserData, 
         signup: <Register name={name} onRegister={onRegister} setIsError={setIsError} />,
         error: <Error />,
         profile: <Profile
-                  name={name}
-                  logOut={logOut}
-                  editUserData={editUserData}
-                  setIsError={setIsError}
-                  isSuccess={isSuccess}
-                  setSuccess={setSuccess}
-                  setIsEdit={setIsEdit}
-                  isEdit={isEdit}
-                />,
+          name={name}
+          logOut={logOut}
+          editUserData={editUserData}
+          setIsError={setIsError}
+          isSuccess={isSuccess}
+          setSuccess={setSuccess}
+          setIsEdit={setIsEdit}
+          isEdit={isEdit}
+        />,
         movies:
           <>
             <Movies savedMovies={savedMovies} addMovie={addMovie} setIsError={setIsError} />
